4c876f83de9bd76274fef6a27590a6d8e68e1876
shijie <shijie@stu.xmu.edu.cn>
2022-10-27 16:42:52 +0800

    init

---
+++ docs/assets/layout.5f8abd28.js
@@ -0,0 +1,1 @@
+import{_ as c,r as e,o as _,c as n,b as o,w as a}from"./index.9c5cd5ff.js";const r={},l={class:"layout"};function i(u,v){const t=e("router-view"),s=e("v-scroll");return _(),n("div",l,[o(s,{class:"main","view-class":"main-scroll-view"},{default:a(()=>[o(t)]),_:1})])}const d=c(r,[["render",i]]);export{d as default};
