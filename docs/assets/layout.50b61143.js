ccf445b2c537d2af21ffdbb5a3bcd83a90fdf809
shijie <shijie@stu.xmu.edu.cn>
2022-11-17 19:54:23 +0800

    update

--- docs/assets/layout.50b61143.js
+++
@@ -1,1 +0,0 @@
-import{p as c,r as e,o as n,h as _,b as o,w as a}from"./index.9c2c3258.js";const r={},l={class:"layout"};function i(u,v){const t=e("router-view"),s=e("v-scroll");return n(),_("div",l,[o(s,{class:"main","view-class":"main-scroll-view"},{default:a(()=>[o(t)]),_:1})])}const p=c(r,[["render",i]]);export{p as default};
